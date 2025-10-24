8234852d658d5d81299fca7f31851c9230e9ef71
P4NDI74 <cervantescervantes2004@gmail.com>
2025-10-24 14:39:10 -0600

    Primer cambio computadora 2

---
+++ app/src/main/java/com/example/proyectomoviles30/ActivityComputadora2.kt
@@ -0,0 +1,20 @@
+package com.example.proyectomoviles30
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityComputadora2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_computadora2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
