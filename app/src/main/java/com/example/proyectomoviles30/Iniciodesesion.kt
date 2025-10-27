6905903905403439cf40ab48302de8eb46475927
AngelGuevara04 <222t0241@itsm.edu.mx>
2025-10-26 18:38:25 -0600

    Inicio de sesión a menú y viceversa listos

--- app/src/main/java/com/example/proyectomoviles30/Iniciodesesion.kt
+++ app/src/main/java/com/example/proyectomoviles30/Iniciodesesion.kt
@@ -8,6 +8,9 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.content.Intent
 import android.widget.Button
+import com.example.proyectomoviles30.util.PreferenceHelper
+import com.example.proyectomoviles30.util.PreferenceHelper.get
+import com.example.proyectomoviles30.util.PreferenceHelper.set
 
 
 class Iniciodesesion : AppCompatActivity() {
@@ -20,6 +23,10 @@ class Iniciodesesion : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        if (preferences["session", false])
+            irMenuInicio()
 
         val textViewIrRegistro = findViewById<TextView>(R.id.textViewIrRegistro)
         textViewIrRegistro.setOnClickListener {
@@ -37,6 +44,14 @@ class Iniciodesesion : AppCompatActivity() {
         }
         private fun irMenuInicio() {
             val intent = Intent(this, MenuInicio::class.java)
+            createSessionPreference()
             startActivity(intent)
+            finish()
         }
+
+        private fun createSessionPreference() {
+            val preferences = PreferenceHelper.defaultPrefs(this)
+            preferences["session"] = true
+        }
+
 }
--- app/src/main/java/com/example/proyectomoviles30/MenuInicio.kt
+++ app/src/main/java/com/example/proyectomoviles30/MenuInicio.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.content.Intent
+import com.example.proyectomoviles30.util.PreferenceHelper
+import com.example.proyectomoviles30.util.PreferenceHelper.set
 
 class MenuInicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,21 @@ class MenuInicio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textViewCerrarSesion = findViewById<TextView>(R.id.textViewCerrarSesion)
+        textViewCerrarSesion.setOnClickListener {
+            clearSessionPreference()
+            irInicioDeSesion()
+        }
+    }
+
+    private fun irInicioDeSesion() {
+        val intent = Intent(this, Iniciodesesion::class.java)
+        startActivity(intent)
+    }
+
+    private fun clearSessionPreference() {
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        preferences["session"] = false
     }
 }
