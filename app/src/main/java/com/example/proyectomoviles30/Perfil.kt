ae795ea95d731f92f7397b29320c335b1ff24759
AngelGuevara04 <222t0241@itsm.edu.mx>
2025-10-27 08:42:01 -0600

    menú perfil implementado y cambio de icono de app

--- app/src/main/java/com/example/proyectomoviles30/Perfil.kt
+++ app/src/main/java/com/example/proyectomoviles30/Perfil.kt
@@ -7,32 +7,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.content.Intent
-import android.widget.Button
 
 
-
-class Registro : AppCompatActivity() {
+class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_perfil)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val textViewIrInicioDeSesion = findViewById<TextView>(R.id.textViewIrInicioDeSesion)
-        textViewIrInicioDeSesion.setOnClickListener {
-            irInicioDeSesion()
+        val textViewIrMenuInicio = findViewById<TextView>(R.id.textViewIrMenuInicio)
+        textViewIrMenuInicio.setOnClickListener {
+            irMenuInicio()
         }
 
-        val buttonIrInicioDeSesion = findViewById<Button>(R.id.buttonIrInicioDeSesion)
-        buttonIrInicioDeSesion.setOnClickListener {
-            irInicioDeSesion()
-        }
     }
-    private fun irInicioDeSesion() {    
-        val intent = Intent(this, Iniciodesesion::class.java)
+    private fun irMenuInicio() {
+        val intent = Intent(this, MenuInicio::class.java)
         startActivity(intent)
     }
 }
